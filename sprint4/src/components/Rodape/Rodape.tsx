4ecefeee7f394724a549832dd933cf7fdea32f1d
Caiocrnyimi <caiocrnyimi@gmail.com>
2024-11-03 19:16:16 -0300

    Rodapé do projeto, dizendo nome do projeto e dos desenvolvedores

---
+++ sprint4/src/components/Rodape/Rodape.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Rodape: React.FC = () => {
+  return (
+    <footer className="rodape">
+      <p>SolviLabs - Sistema de problemas automotivos. Desenvolvido por Caio e Nicolas</p>
+    </footer>
+  );
+};
+
+export default Rodape;
